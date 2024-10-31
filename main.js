6ea5a5575e0902edbd9c67400fa64c36f5aa3932
unknown <juanjesusgiltore7@gmail.com>
2024-10-31 08:54:50 +0100

    2º tarea hecha

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
 import './style.css'
+
+const body=document.querySelector("body")
+const DIVS= document.querySelectorAll("div")
+
+const BUTTON=document.querySelector("button")
+
+BUTTON.className="btn-add"
+
+const DIVSCLASSES={
+  0:"container",
+  1:"search",
+  2:"li-container",
+  3:"empty",
+  4:"task-count"
+}
+
+const creatDivs=(divs)=>{
+  divs.forEach((elementDiv,index) => {
+    elementDiv.className=DIVSCLASSES[index]
+  });
+
+}
+
+console.log(DIVS)
+creatDivs(DIVS)
